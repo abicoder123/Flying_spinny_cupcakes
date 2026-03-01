91502f35f75d223b1952cd5f840be06459337209
abicoder123 <ablor32@icstudents.org>
2026-03-01 11:19:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const cupcake = document.getElementById("cupcake");
 
+cupcake.addEventListener("click", function() {
+    const newCupcake = document.createElement("div");
+    newCupcake.textContent = "🧁";
+    document.body.appendChild(newCupcake);
+});
