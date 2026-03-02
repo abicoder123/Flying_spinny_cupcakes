a64901ca8e576a6f66cd4b77c008bd24b9bbe086
abicoder123 <ablor32@icstudents.org>
2026-03-02 07:02:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const cupcake = document.getElementById("cupcake");
+    const cupcake = document.getElementById("cupcake");
 
-cupcake.addEventListener("click", function() {
-    const newCupcake = document.createElement("span");
-    newCupcake.textContent = "🧁 ";
-    document.body.appendChild(newCupcake);
-});
+    function randomPosition() {
+      const maxWidth = window.innerWidth - 50;
+      const randomX = Math.random() * maxWidth;
+      cupcake.style.left = randomX + "px";
+    }
 
-const cupcake = document.getElementById("cupcake");
+    cupcake.addEventListener("click", function() {
+      const newCupcake = document.createElement("span");
+      newCupcake.textContent = "🧁";
+      document.body.appendChild(newCupcake);
 
-function randomPosition() {
-    const maxWidth = window.innerWidth - 50; 
-    const randomX = Math.random() * maxWidth;
-    cupcake.style.left = randomX + "px";
-}
+      randomPosition(); 
+    });
 
-randomPosition();
+    randomPosition();
